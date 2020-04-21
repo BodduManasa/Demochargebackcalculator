b676481a5deb0c476eae0f3869fa0118fe974256
Devika-Pola <63142005+Devika-Pola@users.noreply.github.com>
2020-04-21 12:47:22 +0530

    updated admin.java

--- src/main/java/com/example/models/Admin.java
+++ src/main/java/com/example/models/Admin.java
@@ -1,5 +1,62 @@
 package com.example.chargebackcalcdemo.models;
 
 public class Admin {
+  
+  String firstname;
+	@Override
+	public String toString() {
+		return "Admin [firstname=" + firstname + ", lastname=" + lastname + ", age=" + age + ", gender=" + gender
+				+ ", mobileno=" + mobileno + ", adminid=" + adminid + ", password=" + password + "]";
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public double getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(double mobileno) {
+		this.mobileno = mobileno;
+	}
+	public int getAdminid() {
+		return adminid;
+	}
+	public void setAdminid(int adminid) {
+		this.adminid = adminid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	String lastname;
+	int age;
+	String gender;
+	double mobileno;
+	int adminid;
+	String password;
+	
+	
 
 }
