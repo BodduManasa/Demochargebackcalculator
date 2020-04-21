e7d2a59ddc9f9d64d5c009bac3157e7cecd8281b
Devika-Pola <63142005+Devika-Pola@users.noreply.github.com>
2020-04-21 14:25:25 +0530

    updated adminlogin.java

--- src/main/java/com/example/models/AdminLogin.java
+++ src/main/java/com/example/models/AdminLogin.java
@@ -1,5 +1,23 @@
 package com.example.chargebackcalcdemo.models;
 
 public class AdminLogin {
+private int adminid;
+	@Override
+public String toString() {
+	return "AdminLogin [adminid=" + adminid + ", password=" + password + "]";
+}
+	public int getAdminid() {
+	return adminid;
+}
+public void setAdminid(int adminid) {
+	this.adminid = adminid;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+	private String password;
 
 }
